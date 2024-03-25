b27c09f0d9ff0a8ee39f8dafaa5d4b23698805f7
Flarnie <fnonemaker@gmail.com>
2024-03-25 14:20:27 -0700

    Add vercel analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <header><h1>Rock Paper Scissors Tournament</h1></header>
       <SpeedInsights />
+      <Analytics />
     </main>
   );
 }

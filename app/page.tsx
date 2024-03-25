e74c7ab23f7991466cdc6d1f60a9de1102810f6e
Flarnie <fnonemaker@gmail.com>
2024-03-25 14:14:07 -0700

    Add speed insights

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <header><h1>Rock Paper Scissors Tournament</h1></header>
+      <SpeedInsights />
     </main>
   );
 }
